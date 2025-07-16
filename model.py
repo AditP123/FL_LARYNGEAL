9249842b5701aad38c3d3d243a6c0fb5d06c94f0
Pushpa <114721876+pushpavalliPI@users.noreply.github.com>
2025-07-16 08:45:58 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,12 +5,13 @@ class LaryngealCNN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(3, 16, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
-            nn.Conv2d(16, 32, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),
+            nn.Conv2d(3, 16, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2),  # 128→64
+            nn.Conv2d(16, 32, 3, padding=1), nn.ReLU(), nn.MaxPool2d(2), # 64→32
             nn.Flatten(),
             nn.Linear(32 * 32 * 32, 128), nn.ReLU(),
             nn.Linear(128, num_classes)
         )
+
 
     def forward(self, x):
         return self.net(x)
